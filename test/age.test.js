21be94369154db3d9ce264b6b027da8246648135
Gabriel <gabrielkn254@gmail.com>
2026-04-17 11:30:17 +0300

    updated working code

--- test/age.test.js
+++ test/age.test.js
@@ -1,12 +1,18 @@
-const { describe, test } = require('node:test');
-const assert = require('node:assert');
 const isOfAge = require('../age');
-// const test = require('node:test');
 
 describe('isOfAge function', () => {
 
-  test('numbers above 21', () => {
-    assert.strictEqual(isOfAge(23), true)
+  test('returns true if age is 21 or older', () => {
+    expect(isOfAge(23)).toBe(true)
+  });
+
+  test('returns true if age is 21', () => {
+    expect(isOfAge(21)).toBe(true)
+  });
+
+  test('returns false if age is below 21 or is a negative number', () => {
+    expect(isOfAge(16)).toBe(false)
+    expect(isOfAge(-21)).toBe(false)
   });
 
 });
